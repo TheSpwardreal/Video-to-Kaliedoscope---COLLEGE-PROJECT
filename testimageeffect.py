df96c2486bd0fd9436608aebc6e4b020e27d4406
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 18:43:56 -0500

    created image resizing thumbnail for cloudsfortest

---
+++ testimageeffect.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+
+def main():
+    with Image.open("cloudsfortest.png") as img:
+        out_img = img.resize((320,180))
+        out_img.save("thumbnail.png")
+
+
+
+if __name__ = "__main__":
+    main()
