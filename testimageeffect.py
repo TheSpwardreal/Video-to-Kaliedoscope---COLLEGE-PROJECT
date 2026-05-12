965c1dbee824a805362df8c6bc649e7d813216b4
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 21:15:59 -0500

    changed foreg_img to forg_img

--- testimageeffect.py
+++ testimageeffect.py
@@ -5,11 +5,11 @@ def main():
     with (Image.open("cloudsfortest.png") as img,
           Image.open("cloudsfortest.png") as overlay_img, 
           Image.open("cloudsfortest.png") as overtop_img):
-        foreg_img = img
+        forg_img = img
         vert_img = overlay_img.transpose(Image.FLIP_LEFT_RIGHT)
         horz_img = overtop_img.transpose(Image.FLIP_TOP_BOTTOM)
         backg_img = vert_img
-        mirrored_img = Image.blend(foreg_img, backg_img, 0.5)
+        mirrored_img = Image.blend(forg_img, backg_img, 0.5)
         out_img = Image.blend(horz_img, mirrored_img, 0.5)
         mirrored_img.show()
         out_img.show()
