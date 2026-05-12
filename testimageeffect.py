31ca2d474d43741ded127cc152d7174531954a09
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 19:41:41 -0500

    created additional overlay image that will be transposed to mirror vertically and overlay with background

--- testimageeffect.py
+++ testimageeffect.py
@@ -2,9 +2,11 @@ from PIL import Image
 
 
 def main():
-    with Image.open("cloudsfortest.png") as img:
+    with (Image.open("cloudsfortest.png") as img,
+          Image.open("cloudsfortest.png") as overlay_img):
         foreg_img = img
-        backg_img = Image.open("cloudsfortest.png")
+        mirror_img = overlay_img.transpose(Image.FLIP_LEFT_RIGHT)
+        backg_img = mirror_img
         out_img = Image.blend(foreg_img, backg_img, 0.5)
         foreg_img.show()
         backg_img.show()
