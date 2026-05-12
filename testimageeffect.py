93fdbd724e7256616098ff5d89dfc4c75c368007
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 21:18:54 -0500

    changed structure to instead blend individual mirrors of each image opposed to overlaying one mirror

--- testimageeffect.py
+++ testimageeffect.py
@@ -9,9 +9,10 @@ def main():
         vert_img = overlay_img.transpose(Image.FLIP_LEFT_RIGHT)
         horz_img = overtop_img.transpose(Image.FLIP_TOP_BOTTOM)
         backg_img = vert_img
-        mirrored_img = Image.blend(forg_img, backg_img, 0.5)
-        out_img = Image.blend(horz_img, mirrored_img, 0.5)
-        mirrored_img.show()
+        mirroredvert_img = Image.blend(forg_img, backg_img, 0.5)
+        mirroredhor_img = Image.blend(forg_img, horz_img, 0.5)
+        out_img = Image.blend(mirroredhor_img, mirroredvert_img, 0.5)
+        mirroredvert_img.show()
         out_img.show()
 
 
