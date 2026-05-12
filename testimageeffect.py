a2fdf16af11326b15ee496ee8fc0a0d6b45b023d
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 19:25:23 -0500

    testing overlay of mirror image, added .show to view results, image.blend to overlay in final product

--- testimageeffect.py
+++ testimageeffect.py
@@ -3,8 +3,14 @@ from PIL import Image
 
 def main():
     with Image.open("cloudsfortest.png") as img:
-        out_img = img.resize((320,180))
-        out_img.save("thumbnail.png")
+        foreg_img = img
+        backg_img = Image.open("cloudsfortest.png")
+        out_img = Image.blend(foreg_img, backg_img, 0.5)
+        foreg_img.show()
+        backg_img.show()
+        out_img.show()
+
+
 
 
 
