5cf185222cdba280ad90ed2a3d7dbff024d091a3
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 21:36:48 -0500

    removed first attempt

--- testimageeffect.py
+++ testimageeffect.py
@@ -10,12 +10,7 @@ def main():
         forg_img = img
         vert_img = overlay_img.transpose(Image.FLIP_LEFT_RIGHT)
         horz_img = overtop_img.transpose(Image.FLIP_TOP_BOTTOM)
-        backg_img = vert_img
-        mirroredvert_img = Image.blend(forg_img, backg_img, 0.5)
-        mirroredhor_img = Image.blend(forg_img, horz_img, 0.5)
-        out_img = Image.blend(mirroredhor_img, mirroredvert_img, 0.5)
-        mirroredvert_img.show()
-        out_img.show()
+        
 
 
 
