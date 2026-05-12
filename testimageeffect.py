b992554819e5f5c46968c9f0e48673ebce9f51cd
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 22:06:33 -0500

    restructured range of pixels in order to properly record what sections will be mirrored vertically and horizontally

--- testimageeffect.py
+++ testimageeffect.py
@@ -1,16 +1,21 @@
 from PIL import Image
 
 
+
+
 def main():
     with (Image.open("penlo3d.png") as img,
           Image.open("penlo3d.png") as overlay_img, 
           Image.open("penlo3d.png") as overtop_img):
-        x = img.width // 2
-        y = img.height // 2
-        forg_img = img # original image
+        out_img = img.copy()
         vert_img = overlay_img.transpose(Image.FLIP_LEFT_RIGHT) # vertically mirrored image
         horz_img = overtop_img.transpose(Image.FLIP_TOP_BOTTOM) # horizontally mirrored image
-        forg_img.paste(vert_img, (x, y), mask=vert_img.crop(x,0)) # merging of vertical onto original
+        for y in range(img.height):
+            for x in range(img.width//2): # 2nd in heirarchy is half the pixel width
+                pix = img.getpixel((x,y)) # grabbing the pixel ratios in order to measure it when applying
+                out_img.paste((x,y), vert_img(pix), horz_img(pix)) # applying vertical / horizontals
+        forg_img = img # original image
+        
         img.show()
 
 
