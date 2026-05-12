fd00e9c63e6fa3345031083d794d1a9467ddf87a
Gabriel Manquero <gabrielmanquero1@gmail.com>
2026-05-11 21:34:02 -0500

    added x y coordinates as halfway marks on where vertical / horizontal crop shall begin

--- testimageeffect.py
+++ testimageeffect.py
@@ -5,6 +5,8 @@ def main():
     with (Image.open("penlo3d.png") as img,
           Image.open("penlo3d.png") as overlay_img, 
           Image.open("penlo3d.png") as overtop_img):
+        x = img.width // 2
+        y = img.height // 2
         forg_img = img
         vert_img = overlay_img.transpose(Image.FLIP_LEFT_RIGHT)
         horz_img = overtop_img.transpose(Image.FLIP_TOP_BOTTOM)
